f93c95200d838003887084c2dce03756a46d2c04
karimzade_k <karimzade_k@STHQ0118-01>
2025-09-16 21:28:07 +0400

    ThreadSafeSingleton added

---
+++ ThreadSafeSingleton/Program.cs
@@ -0,0 +1,43 @@
+﻿#nullable disable
+
+
+
+// President p0 = new(); // inaccessible
+
+
+President p1 = President.GetInstance();
+President p2 = President.GetInstance();
+
+
+Console.WriteLine(p1.GetHashCode());
+Console.WriteLine(p2.GetHashCode());
+
+
+class President
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Height { get; set; }
+
+    private President() { }
+
+    private static President _instance;
+    private static object _lock = new object();
+
+
+    public static President GetInstance()
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new President { Name = "XXX", Surname = "XXX", Height = 195 };
+                }
+            }
+        }
+
+        return _instance;
+    }
+}
